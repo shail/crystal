5bb810c65b048afd37b005c44dc29fa64a78de87
Shail <shailpatel2@gmail.com>
2016-08-05 17:35:34 -0700

    initial teams index method

--- src/supercontest_data_crystal.cr
+++ src/supercontest_data_crystal.cr
@@ -1,8 +1,15 @@
 require "./supercontest_data_crystal/*"
 require "kemal"
+require "pg"
+require "json"
 
-get "/" do
-  "Hello World!"
+DB = PG.connect("postgres://shailpatel@localhost:5432/supercontest_data")
+
+get "/" do |env|
+  env.response.content_type = "application/json"
+  result = DB.exec({Int32, String}, "SELECT id, name FROM teams")
+  hash = { "data" => result.to_hash }
+  hash.to_json
 end
 
 Kemal.run
